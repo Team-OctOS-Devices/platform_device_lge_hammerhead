2bdb4017862a843b2ec255e7d73b2f1cdf1cb261
hedwig34 <hedwig34@gmail.com>
2015-08-17 15:54:45 -0400

    hammerhead: Add Team OctOs vendor calls
    Change-Id: I061343c836e061b41c5a743d477fe7172e3cc07a

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -23,12 +23,18 @@ PRODUCT_COPY_FILES := device/lge/hammerhead/apns-full-conf.xml:system/etc/apns-c
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Include Team OctOs common configuration
+include vendor/to/config/main.mk
+
+# Include Team OctOs telephony configuration
+include vendor/to/config/telephony.mk
+
 PRODUCT_NAME := full_hammerhead
 PRODUCT_DEVICE := hammerhead
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on HammerHead
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
